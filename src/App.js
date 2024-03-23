cd795bd79d6fc4188223c35774d087f9adcbac59
rod773 <rodrigoarcadio1@gmail.com>
2024-03-23 14:13:57 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand navbar-light fixed-top">
+        <div className="container">
+          <a href="" className="navbar-brand">Home</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link">Login</a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">Sign</a>
+              </li>
+              </ul>
+          </div>
+        </div> 
+      </nav>
     </div>
   );
 }
